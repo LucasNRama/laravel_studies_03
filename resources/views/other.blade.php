8692f5eb2e7a8ea9fa4355ebcc7c0bd05cb6691d
LucasNRama <LucasNRama@gihub.com>
2024-11-22 14:26:45 -0300

    master

---
+++ resources/views/other.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.other_layout')
+
+@section('page_title', 'Other Page')
+@section('top_bar')
+    @parent
+    <div>Conteúdo adicional da barra de topo</div>
+@endsection
+
+
+@section('content')
+    <p>Conteúdo da minha View</p>
+@endsection
+
+
+@section('botton_bar')
+    <p>Conteúdo da barra de rodapé</p>
+@endsection
