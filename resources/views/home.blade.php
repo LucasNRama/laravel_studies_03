25fd80222789c4ab52dea46ac5b91fa7d57cfbb3
LucasNRama <LucasNRama@gihub.com>
2024-11-22 14:48:48 -0300

    001

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.main_layout')
 
-@section('page_title', 'Home Page')
-@section('content')
-    <p>Conteúdo da minha View</p>
-@endsection
+@section('page_layout', 'Home page')
 
+@section('content')
+<div class="conteiner mt-5">
+    <div class="row">
+        <div class="col card p-5 bg-secondary text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::view('/other', 'other');
+Route::view('/home', 'home');
+
