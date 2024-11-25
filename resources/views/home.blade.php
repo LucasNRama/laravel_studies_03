e1bceb6a3f6643b494547996b940265155fcd4bc
root <root@EST1004649.sms.fortaleza>
2024-11-25 15:01:53 -0300

    007

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.main_layout')
 @section('content')
 
+{{-- 
 <div class="text-center">
     @foreach ($pessoas_linguas as $pessoa => $linguas)
 
-        {{-- component --}}
         <x-card_pessoa :$pessoa :$linguas />
         
     @endforeach
 </div>
+--}
 
+{{-- componentes e slots --}}
+<div>
+    <h4 class="text-info">Como funciona um Slot?</h4>
+    <x-other-card>
+        <h1 class="text-danger">Este é o Slot!</h1>
+    </x-other-card>
+</div>
 @endsection
